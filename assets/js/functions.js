c5daeab3e574f712b4fd64ab02b1a0afe3d1aaed
Daniel <danieldarvile@MacBook-Pro-de-Daniel.local>
2015-06-07 20:18:21 -0300

    Menu animated ready mobile and desktop

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -6,4 +6,17 @@
     * @param {type} parameter - description.
     */
 
+    /**
+    * Menu animation effect
+    * Description
+    * @constructor
+    * @param {type} parameter - description.
+    */
+
+    $('#toggle').on('click', function () {
+    	var lines = $(this).find('.lines');
+    	$(lines).toggleClass('lines-after lines-before');
+    	$('#menu').toggleClass('open');
+    });
+
 } )( jQuery );
